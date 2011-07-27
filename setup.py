5d8f75ab2c233c2a00ab6886361b4b988aba5f91
Johannes Lötzsch <git_donotspam@johannesloetzsch.de>
2011-07-27 20:57:37 +0400

    more information in setup (for egg-info)

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ import subprocess
 sys.argv.append('install')
 setup(name='ipy_flora',
       version='0.1',
+      licence='GPL',
+      platforms='should be platform-independent — when you have all dependencies (see INSTALL)',
       url='https://github.com/johannesloetzsch/ipy_flora',
       author='Johannes Lötzsch',
       author_email='github_donotspam_at_johannesloetzsch.de',
+      description = open('README').readline().strip(),
+      long_description = ''.join(open('README').readlines()[1:]).strip(),
       py_modules = [ 'ipy_flora.rpsimple', 'ipy_flora.ipy_flora' ]
 	 )
 
